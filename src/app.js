cca546f7b76e65cee63e4691ad50b2cce0fba9db
Ayushkant3011 <ayushkant17@gmail.com>
2026-06-17 16:07:01 +0530

    server added and express installed

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log("Project DevTinder");
+const express = require("express");
+
+const app = express();
+
+
+app.use((req,res)=>{
+    res.send("Hello from the server !");
+});
+
+app.listen(3000, ()=>{
+    console.log("Server is running");
+});
