fed39b2b3001fda1f4a6a749fe505088074cee22
Ayushkant3011 <ayushkant17@gmail.com>
2026-06-17 16:18:45 +0530

    nodemon added

--- src/app.js
+++ src/app.js
@@ -3,7 +3,13 @@ const express = require("express");
 const app = express();
 
 
-app.use((req,res)=>{
+
+app.use("/",(req,res)=>{
+    res.send("Hello from the Home !");
+});
+
+
+app.use("/test",(req,res)=>{
     res.send("Hello from the server !");
 });
 
