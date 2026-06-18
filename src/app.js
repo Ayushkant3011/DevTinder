a6b8a712f9bf6e425b8c7a43b2d22aeeff38c3fb
Ayushkant3011 <ayushkant17@gmail.com>
2026-06-18 16:28:08 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -3,15 +3,22 @@ const express = require("express");
 const app = express();
 
 
-
-app.use("/",(req,res)=>{
+app.use("/home",(req,res)=>{
     res.send("Hello from the Home !");
 });
+
 
 
 app.use("/test",(req,res)=>{
     res.send("Hello from the server !");
 });
+
+
+
+app.use("/",(req,res)=>{
+    res.send("Hello from the Dashboard !");
+});
+
 
 app.listen(3000, ()=>{
     console.log("Server is running");
