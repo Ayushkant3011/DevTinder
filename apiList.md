8bdf84607ceba419bd09f1c6d0c64ca227ded2d4
Ayushkant3011 <ayushkant17@gmail.com>
2026-07-30 14:58:23 +0530

    sendRequest api added

--- apiList.md
+++ apiList.md
@@ -12,8 +12,9 @@
 - PATCH /profile/password
 
 ### connectionRequestRouter
-- POST /request/send/interested/:userId
-- POST /request/send/ignored/:userId
+<!-- - POST /request/send/interested/:userId //making it dynamic -->
+- POST /request/send/:status/:userId //making it dynamic
+<!-- - POST /request/send/ignored/:userId -->
 - POST /request/review/accepted/:requestId
 - POST /request/review/rejected/:requestId
 
--- src/routes/request.js
+++ src/routes/request.js
@@ -2,12 +2,39 @@ const express = require("express");
 const requestRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
 
-requestRouter.post("/sendRequest", userAuth, async (req, res) =>{
-    const user = req.user;
-    console.log("Sending connection request");
 
-    res.send(user.firstName + " Sent Connection Request !");
+requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) =>{
+    try{
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
+
+
+        const allowedStatus = ["ignored", "interested"];
+        if(!allowedStatus.includes(status)){
+            return res.status(400).json({message: "Invalid Status Type" + status});
+        }
+        
+        const connectionRequest = new ConnectionRequest({
+            fromUserId,
+            toUserId,
+            status,
+        });
+
+
+        const requestData = await connectionRequest.save();
+
+
+        res.json({
+            message: "conenction Request Sent Successfully !!",
+            requestData,
+        })
+    }
+    catch(err){
+        res.status(400).send("ERROR : " + err.message);
+    }
 });
 
 
