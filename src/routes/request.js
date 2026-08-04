d3ed5b8540e4d55b02affa0dac873cf50367ef43
Ayushkant3011 <ayushkant17@gmail.com>
2026-08-04 15:13:04 +0530

    More validations added to connectionRequest

--- src/routes/request.js
+++ src/routes/request.js
@@ -3,6 +3,7 @@ const requestRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 
 requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) =>{
@@ -16,7 +17,28 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
         if(!allowedStatus.includes(status)){
             return res.status(400).json({message: "Invalid Status Type" + status});
         }
+
+        const toUser = await User.findById(toUserId);
+
+        if(!toUser){
+            return res.status(404).json({message: "User Does not Exists"});
+        }
         
+        // Check if there is an existing connectionRequest
+        const existingConnectionRequest = await ConnectionRequest.findOne({
+            $or: [
+                {fromUserId, toUserId,},
+                {fromUserId: toUserId, toUserId: fromUserId}
+            ]
+        });
+
+        if(existingConnectionRequest){
+            return res
+                .status(400)
+                .send({message: "Connection request already exists"});
+        }
+
+
         const connectionRequest = new ConnectionRequest({
             fromUserId,
             toUserId,
