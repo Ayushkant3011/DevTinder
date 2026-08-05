93ec347bc6d359d3c75d8c6a580419cb6479cad1
Ayushkant3011 <ayushkant17@gmail.com>
2026-08-05 12:55:57 +0530

    Pre middleware added

--- src/routes/request.js
+++ src/routes/request.js
@@ -50,7 +50,7 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
 
 
         res.json({
-            message: "conenction Request Sent Successfully !!",
+            message: req.user.firstName + " is " + status + " in " + toUser.firstName,
             requestData,
         })
     }
