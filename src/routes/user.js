aede44ceb233bc761f1e8b486c6b5e79796d2bad
Ayushkant3011 <ayushkant17@gmail.com>
2026-08-13 14:52:38 +0530

    FeedAPI pagination added

--- src/routes/user.js
+++ src/routes/user.js
@@ -65,6 +65,12 @@ userRouter.get("/feed", userAuth, async(req,res)=>{
     try{
         const loggedInUser = req.user;
 
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit  = limit > 50 ? 30 : limit;
+
+        const skip = (page - 1) * limit;
+
         // find all connection request (sent + received)
         const connectionRequest = await ConnectionRequest.find({
             $or:[
@@ -85,7 +91,7 @@ userRouter.get("/feed", userAuth, async(req,res)=>{
                 { _id: { $nin: Array.from(hideUsersFromFeed) } },
                 { _id: { $ne : loggedInUser._id}},
             ],
-        }).select(USER_SAFE_DATA);
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
 
 
         res.send(users);
