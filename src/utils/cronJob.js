293cf6ac9e48e55a6908fab191e3ac490d3847a7
Ayushkant3011 <ayushkant17@gmail.com>
2026-09-08 16:03:30 +0530

    Cron job created and emails

--- src/utils/cronJob.js
+++ src/utils/cronJob.js
@@ -1,21 +1,40 @@
 const cron = require("node-cron");
 const {subDays, startOfDay, endOfDay} = require("date-fns");
 const ConnectionRequestModel = require("../models/connectionRequest");
+const sendEmail = require("./sendEmail");
 
-cron.schedule("* * * * * *" , ()=>{
+cron.schedule("* * * * * *" , async ()=>{
     // Send Emails to all the people who got requests previous day
     try{
         const yesterday = subDays(new Date(), 1);
         const yesterdayStart = startOfDay(yesterday);
         const yesterdayEnd = endOfDay(yesterday);
 
-        const pendingRequests = ConnectionRequestModel.find({
+        const pendingRequests = await ConnectionRequestModel.find({
             status: "interested",
             createdAt: {
                 $gte : yesterdayStart,
                 $lt : yesterdayEnd,
             },
-        }).populate("fromUserId toUserId") ;
+        }).populate("fromUserId toUserId");
+
+
+        const listOfEmails = [...new Set(pendingRequests.map(req => req.toUserId.emailId))];
+
+        for(const email of listOfEmails){
+            // Send Emails
+            try{
+                const res = await sendEmail.run(
+                    "New Friend Requests Pending " + email,
+                    "There are so many friend requests pending for you, please login to DevTinder and Manage your requests"
+                );
+
+                console.log(res);
+            }
+            catch(err){
+                console.error(err);
+            }
+        }
     }
     catch(err){
         console.error(err);
