2cc15379c9339b53eb0bb795c05fb06e074b7d2f
Ayushkant3011 <ayushkant17@gmail.com>
2026-09-14 18:26:48 +0530

    Bug Fix

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -95,7 +95,7 @@ paymentRouter.post("/payment/webHook", async(req, res)=>{
 
 
 paymentRouter.get("/premium/verify", userAuth, async(req,res)=>{
-    const user = req.user;
+    const user = req.user.toJSON();
 
     if(user.isPremium) return res.json({ ispremium: true });
 
