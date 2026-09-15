317509488a8fd2894b0914fff3b0c6246cdf89d0
Ayushkant3011 <ayushkant17@gmail.com>
2026-09-15 18:37:32 +0530

    socket connection made and chat feat is working

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,12 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
 
+const getSecretRoomId = (userId, targetUserId)=>{
+    return crypto
+        .createHash("sha256")
+        .update([userId, targetUserId].sort().join("_"))
+        .digest("hex");
+};
 
 const initializeSocket = (server) =>{
     const io = socket(server, {
@@ -13,7 +20,28 @@ const initializeSocket = (server) =>{
 
     io.on("connection", (socket) =>{
         // Handle Events
+
+        socket.on("joinChat", ({firstName, userId, targetUserId})=>{
+            const roomId = getSecretRoomId(userId, targetUserId);
+            
+            console.log(firstName + " Joined room :" + roomId);
+
+            socket.join(roomId);
+        });
+
+        socket.on(
+            "sendMessage", 
+            ({firstName, userId, targetUserId, text })=>{
+                const roomId = getSecretRoomId(userId, targetUserId);
+                console.log(firstName + " " + text);
+                io.to(roomId).emit("MessageReceived", { firstName, text});
+
+        });
+
+        socket.on("disconnect", ()=>{
+
+        });
     });
-}
+};
 
 module.exports = initializeSocket;
